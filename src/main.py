ccc9bd2562c6c5ebde3d0a989be569d3b245bd17
Berkee <berkeeavcii@gmail.com>
2021-08-07 20:39:33 +0300

    v0.2

--- src/main.py
+++ src/main.py
@@ -4,4 +4,4 @@ fileWritecommand = "a"
 Brango(fileName, fileWritecommand).CreateFile()
 Brango(fileName, fileWritecommand).WriteFile("dd = document.getElementById('Berke');")
 Brango(fileName, fileWritecommand).FindIdElement("bb", "Berke")
-Brango(fileName, fileWritecommand).FindIdWrite("sg", "Berke", "MERHABA DUNYA")
+Brango(fileName, fileWritecommand).FindIdWrite("sg", "Berke", "DOLAR")
